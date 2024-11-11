77dbf4b8313c550eededc2627844248697295d67
uraxxx <inkogg.neato@gmail.com>
2024-11-12 00:08:28 +0100

    test2

--- userconfig.js
+++ userconfig.js
@@ -16,7 +16,7 @@ const default_config = {
   openLastVisitedTab: true,
   tabs: [
     {
-      name: "myself",
+      name: "jagsjälv",
       background_url: "src/img/banners/cbg-9.gif",
       categories: [
         {
